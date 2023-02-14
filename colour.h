fa472ad820e32fb458472d43dabcbb81cd1dd022
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2023-02-14 15:45:37 +0000

    Formatting

--- colour.h
+++ colour.h
@@ -1,26 +1,25 @@
 #ifndef COLOUR_H
 #define COLOUR_H
 
-class Colour
-{
-public:
-    unsigned int r;
-    unsigned int g;
-    unsigned int b;
+class Colour {
+ public:
+  unsigned int r;
+  unsigned int g;
+  unsigned int b;
 
-    template<typename T>
-    static Colour from_rgb(T r_, T g_, T b_);
+  template<typename T>
+  static Colour from_rgb(T r_, T g_, T b_);
 
-    template<typename T>
-    static Colour from_brightness(T grey);
-    template<typename T>
-    static Colour from_rgb_float(T r_, T g_, T b_);
+  template<typename T>
+  static Colour from_brightness(T grey);
+  template<typename T>
+  static Colour from_rgb_float(T r_, T g_, T b_);
 
-    template<typename T>
-    static Colour from_hsv(T h, T s, T v);
+  template<typename T>
+  static Colour from_hsv(T h, T s, T v);
 
-    template<typename T>
-    static Colour viridis(const T v);
+  template<typename T>
+  static Colour viridis(const T v);
 };
 
 
--- complex.h
+++ complex.h
@@ -1,24 +1,25 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 
+// Complex number
+
 template<typename T>
-class Complex // Complex number
-{
-public:
-    T r, i;
-    Complex();
-    Complex(T r, T i);
-    Complex operator+(const Complex& c) const;
-    Complex operator-(const Complex& c) const;
-    void operator+=(const Complex& c);
-    void operator-=(const Complex& c);
-    void operator*=(const Complex& c);
-    Complex operator/(float s);
+class Complex {
+ public:
+  T r, i;
+  Complex();
+  Complex(T r, T i);
+  Complex operator+(const Complex& c) const;
+  Complex operator-(const Complex& c) const;
+  void operator+=(const Complex& c);
+  void operator-=(const Complex& c);
+  void operator*=(const Complex& c);
+  Complex operator/(float s);
 
 
-    void display();
-    T magnitude_squared();
-    T magnitude();
+  void display();
+  T magnitude_squared();
+  T magnitude();
 };
 
 #endif // COMPLEX_H
